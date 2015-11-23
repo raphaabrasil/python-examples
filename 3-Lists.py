c981f6025fbdf3a1ad274292e3e1a3216cb6f69a
Raphael Brasil <raphael.brasil@corp.globo.com>
2015-11-23 11:18:31 -0200

    Finaliza exemplos de listas

--- 3-Lists.py
+++ 3-Lists.py
@@ -1,16 +1,33 @@
+# coding:utf-8
+
 ''' 3-Lists.py '''
 
 my_list = []
 my_other_list = [0, 1, 2]
+print(my_list)  # []
+print(my_other_list)  # 0, 1, 2
+
+my_other_list.extend([3, 4, 5])
+print(my_other_list)  # 0, 1, 2, 3, 4, 5
+
+my_other_list[3:] = []
+print(my_other_list)  # 0, 1, 2
+
+my_other_list = my_other_list + [3, 4]
+print(my_other_list)  # 0, 1, 2, 3, 4
+
+my_other_list.remove(4)  # 0, 1, 2, 3
+my_other_list.pop(3)  # 0, 1, 2
+print(my_other_list)
 
 ''' Pegadinha '''
 my_list = my_other_list
-print(my_list)
+print(my_list)  # 0, 1, 2
 
 my_list.append(135)
 
-print(my_list)  # !!!
-print(my_other_list)  # ???
+print(my_list)  # 0, 1, 2, 135 (!!!)
+print(my_other_list)  # 0, 1, 2, 135 (???)
 
 '''
 Quando copiamos uma list usando list = list 2,
@@ -22,6 +39,7 @@ essa mudança também ocorre na outra :)
 Como fazer para isso nao acontecer?
 '''
 
+''' Opção 1 '''
 my_list = []
 my_other_list = [0, 1, 2]
 
@@ -30,5 +48,17 @@ print(my_list)
 
 my_list.append(135)
 
-print(my_list)  # !!!
-print(my_other_list)  # !!!!!!
+print(my_list)  # 0, 1, 2, 135 (!!!)
+print(my_other_list)  # 0, 1, 2 (!!!!!!)
+
+''' Opção 2 '''
+my_list = []
+my_other_list = [0, 1, 2]
+
+my_list = my_other_list[:]  # !!!
+print(my_list)
+
+my_list.append(135)
+
+print(my_list)  # 0, 1, 2, 135 (!!!)
+print(my_other_list)  # 0, 1, 2 (!!!!!!)
