3e3d91b1e34bc01b3b2af02e5401da6fcf7177b7
Raphael Brasil <raphael.brasil@corp.globo.com>
2015-12-01 15:20:47 -0200

    Adiciona exemplos de inclusao e remocao em dict

--- 5-Dict.py
+++ 5-Dict.py
@@ -2,13 +2,22 @@
 
 dados = {"nome": "Raphael", "idade": 22}
 
-dados["nome"]                 # "Raphael"
-dados["idade"] += 1           # 23
+dados["nome"]                  # "Raphael"
+dados["idade"] += 1            # 23
 
-"nome" in dados               # True
-"sobrenome" in dados		  # False
+"nome" in dados                # True
+"sobrenome" in dados		   # False
 
-dados.get("likes", 0)         # Se a chave não existir, retorna 0
+dados.get("likes", 0)          # Se a chave não existir, retorna 0
 
-dados.keys()                  # ["nome", "idade"]
-dados.values()                # ["Raphael", 23]
+dados.keys()                   # ["nome", "idade"]
+dados.values()                 # ["Raphael", 23]
+
+dados['sobrenome'] = 'Brasil'  # Adicionando uma chave no dict
+
+dados.keys()                   # ["idade", "nome", "sobrenome"]
+dados.values()                 # [23, "Raphael", "Brasil"]
+
+del dados['sobrenome']		   # Removendo uma chave do dict
+
+dados.keys()                   # ["idade", "nome"]
